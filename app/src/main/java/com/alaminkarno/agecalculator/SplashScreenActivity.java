509374d0f4a00444b21a187d2d342a319b78e90d
Karno786 <amin15-1951@diu.edu.bd>
2022-05-02 23:21:53 +0600

    Splash Screen - Age Calculator

---
+++ app/src/main/java/com/alaminkarno/agecalculator/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.alaminkarno.agecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_SCREEN_TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME);
+    }
+}
